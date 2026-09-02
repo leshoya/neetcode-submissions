40011facc1ace7165211b580b926e1cf25623262
Sophia L <sophial23041@gmail.com>
2026-09-01 21:32:57 -0400

    Add: fruit-into-baskets - submission-2

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-2.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        left = 0
+        counts = defaultdict(int)
+        res = 0
+
+        for r in range(len(fruits)):
+            counts[fruits[r]] += 1
+
+            while len(counts) > 2:
+                counts[fruits[left]] -= 1
+                if counts[fruits[left]] == 0:
+                    del counts[fruits[left]]
+                left += 1
+
+            res = max(res, r - left + 1)
+
+        return res
